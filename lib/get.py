89d6a65276f08a387ed5fca4254f17da5180343b
Mheyrie <omota4fidelity@gmail.com>
2023-11-03 11:47:58 +0100

    done

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,14 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+url = "https://moringa.instructure.com/courses/356/pages/useful-reading-and-exploration?module_item_id=64467"
 
 response = requests.get(url)
 
-json_content = json.loads(response.text)
+# json_content = json.loads(response.content)
 
-print(json.dumps(json_content, indent=4))
+# print(json.dumps(json_content, indent=4, sort_keys=True))
+print(response.text)
+# print(response.content)
+# print(json_content)
